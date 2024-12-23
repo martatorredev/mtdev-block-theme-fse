762c59a4bd1d29747e9212b05712dd0fca15583e
martatorredev <hola@martatorre.dev>
2024-12-23 01:00:24 +0100

    Update theme.json: change typography to IBM Plex Sans

--- functions.php
+++ functions.php
@@ -11,3 +11,13 @@ if ( ! function_exists( 'mtdev_theme_setup' ) ) {
     }
 }
 add_action( 'after_setup_theme', 'mtdev_theme_setup' );
+function mtdev_enqueue_styles() {
+    wp_enqueue_style(
+        'mtdev-google-fonts',
+        'https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;500;700&display=swap',
+        array(),
+        null
+    );
+    wp_enqueue_style('mtdev-style', get_stylesheet_uri());
+}
+add_action('wp_enqueue_scripts', 'mtdev_enqueue_styles');
