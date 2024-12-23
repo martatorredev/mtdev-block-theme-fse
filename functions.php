6b43b9cc598b6cf326d4225e97ab050632c5165e
martatorredev <hola@martatorre.dev>
2024-12-23 01:03:19 +0100

    Update typography to Space Grotesk for titles and Red Hat Display for body

--- functions.php
+++ functions.php
@@ -1,23 +1,12 @@
 <?php
-// Theme setup
-if ( ! function_exists( 'mtdev_theme_setup' ) ) {
-    function mtdev_theme_setup() {
-        // Enable support for block templates
-        add_theme_support( 'block-templates' );
-
-        // Add support for editor styles
-        add_theme_support( 'editor-styles' );
-        add_editor_style( 'style.css' );
-    }
-}
-add_action( 'after_setup_theme', 'mtdev_theme_setup' );
 function mtdev_enqueue_styles() {
     wp_enqueue_style(
         'mtdev-google-fonts',
-        'https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;500;700&display=swap',
+        'https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;700&family=Red+Hat+Display:wght@400;500;700&display=swap',
         array(),
         null
     );
     wp_enqueue_style('mtdev-style', get_stylesheet_uri());
 }
 add_action('wp_enqueue_scripts', 'mtdev_enqueue_styles');
+
